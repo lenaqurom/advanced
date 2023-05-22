b0aa8cbdf8fc286bbc2820f8636a36ad8a633847
[[sally-hn]] <[[sally.nanaa25@gmail.com]]>
2023-05-22 22:21:13 +0300

    FINAL+Decryption

--- routes/deleteToken.js
+++
@@ -1,9 +0,0 @@
-// delete token
-userSchema.methods.deleteToken=function(token,cb){
-    var user=this;
-
-    user.update({$unset : {token :1}},function(err,user){
-        if(err) return cb(err);
-        cb(null,user);
-    })
-}
--- routes/userlogin.js
+++ routes/userlogin.js
@@ -3,37 +3,35 @@ const jwt = require('jsonwebtoken');
 const config = require('../config');
 const User = require('../moduls/User');
 const router = express.Router();
+const bcryptjs = require('bcryptjs');
 
 
 // Define the login route
 router.post('/userlogin', async (req, res, next) => {
   const { username, password } = req.body;
+  
+  const user = await User.findOne({ username });
 
-  try {
-    // Retrieve user from the database
-    const user = await User.findOne({ username });
-
-    // Check if user exists
-    if (!user) {
-      return res.status(401).json({ error: 'Invalid credentials' });
-    }
-
-    // Verify password
-    if (password !== user.password) {
-      return res.status(401).json({ error: 'Invalid credentials' });
-    }
-
-    // Generate JWT
-    const token = jwt.sign({ userId: user._id }, config.secretKey, { expiresIn: '1h' });
-
-    // Return JWT to the client
-    res.json({ token });
-  } catch (error) {
-    next(error);
+  if(!user){
+      return res.status(400).json({msg : "User with this username does not exit !"});
   }
-});
 
+  const isMatch = await bcryptjs.compare(password, user.password);
+  if(isMatch){
+    return res.status(200).json({msg : "Succefull LOGIN, correct password!"});
+}
 
+  if(!isMatch){
+      return res.status(400).json({msg : "Incorrect password!"});
+  }
+
+  const token = jwt.sign({id: user._id}, "passwordKey");
+
+  res.json({token, ...user._doc});
+
+}
+
+)
 
 
 // Export the router
